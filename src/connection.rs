59e36ea6d30ab7d729eade592bf17322d9deded9
Oleh Pykylytsky <oleh@mbp.local>
2025-07-25 17:24:29 +0300

    codecrafters submit [skip ci]

--- src/connection.rs
+++ src/connection.rs
@@ -9,11 +9,11 @@ use std::{
     time::Duration,
 };
 use thiserror::Error;
+use tokio::io::AsyncWriteExt;
 use tokio::io::{self, AsyncRead};
 use tokio::io::{AsyncReadExt, AsyncWrite};
 use tokio::net::TcpStream;
 use tokio::sync::RwLock;
-use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::{
     command::{
@@ -187,12 +187,19 @@ impl<'s> Connection<'s> {
                     self.server_replication_id
                 )));
                 self.write_all(&fullresync.encode()).await?;
+                let empty_rdb: &[u8] = &[
+                    0x52, 0x45, 0x44, 0x49, 0x53, 0x30, 0x30, 0x31, 0x31, 0xfa, 0x09, 0x72, 0x65,
+                    0x64, 0x69, 0x73, 0x2d, 0x76, 0x65, 0x72, 0x05, 0x37, 0x2e, 0x32, 0x2e, 0x30,
+                    0xfa, 0x0a, 0x72, 0x65, 0x64, 0x69, 0x73, 0x2d, 0x62, 0x69, 0x74, 0x73, 0xc0,
+                    0x40, 0xfa, 0x05, 0x63, 0x74, 0x69, 0x6d, 0x65, 0xc2, 0x6d, 0x08, 0xbc, 0x65,
+                    0xfa, 0x08, 0x75, 0x73, 0x65, 0x64, 0x2d, 0x6d, 0x65, 0x6d, 0xc2, 0xb0, 0xc4,
+                    0x10, 0x00, 0xfa, 0x08, 0x61, 0x6f, 0x66, 0x2d, 0x62, 0x61, 0x73, 0x65, 0xc0,
+                    0x00, 0xff, 0xf0, 0x6e, 0x3b, 0xfe, 0xc0, 0xff, 0x5a, 0xa2,
+                ];
                 let mut rdb = vec![];
-                File::open("dump.rdb").await?.read_buf(&mut rdb).await?;
-                let mut resync = vec![];
-                resync.extend_from_slice(format!("${}\r\n", rdb.len()).as_bytes());
-                resync.extend_from_slice(rdb.as_slice());
-                self.write_all(&resync).await?;
+                rdb.extend_from_slice(format!("${}\r\n", empty_rdb.len()).as_bytes());
+                rdb.extend_from_slice(empty_rdb.as_slice());
+                self.write_all(&rdb).await?;
                 return Ok(());
             }
         };

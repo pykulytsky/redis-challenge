4d56ee1cfe5221dd5dc056f735c57e4cb158fa73
Oleh Pykylytsky <oleh@mbp.local>
2025-07-26 10:23:44 +0300

    codecrafters submit [skip ci]

--- src/replica.rs
+++ src/replica.rs
@@ -142,13 +142,16 @@ impl Replica {
                 }
             }
             Command::ReplConf(key, _value) => {
-                if matches!(key, Resp::BulkString(Cow::Borrowed("GETACK"))) {
-                    let resp: Resp<'_> = Command::ReplConf(
-                        Resp::bulk_string("ACK"),
-                        Resp::BulkString(Cow::Owned(self.bytes_processed.to_string())),
-                    )
-                    .into();
-                    tcp.write_all(&resp.encode()).await?;
+                let key = key.expect_bulk_string().map(|key| key.clone().into_owned());
+                if let Some(key) = key {
+                    if key.as_str() == "GETACK" {
+                        let resp: Resp<'_> = Command::ReplConf(
+                            Resp::bulk_string("ACK"),
+                            Resp::BulkString(Cow::Owned(self.bytes_processed.to_string())),
+                        )
+                        .into();
+                        tcp.write_all(&resp.encode()).await?;
+                    }
                 }
             }
             _ => {

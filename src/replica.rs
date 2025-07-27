38234e5ca645ae7dbd12f51c7ab4f92099a2527f
Oleh Pykylytsky <oleh@mbp.local>
2025-07-27 11:51:11 +0300

    codecrafters submit [skip ci]

--- src/replica.rs
+++ src/replica.rs
@@ -128,11 +128,12 @@ impl Replica {
                                 rest.len() - new_rest.len(),
                                 &c
                             );
-                        }
-                        self.handle_command(c, &mut tcp).await?;
-                        if should_account {
                             self.bytes_processed += rest.len() - new_rest.len();
                         }
+                        self.handle_command(c, &mut tcp).await?;
+                        // if should_account {
+                        //     self.bytes_processed += rest.len() - new_rest.len();
+                        // }
                         if is_write_command {
                             let ack: Resp<'_> = Command::ReplConf(
                                 Resp::bulk_string("ACK"),
